2abe0af892041feb9a8042f39aeab32c0ce51482
Tadas Ščerbickas <t.scerbickas@baltic-amadeus.lt>
2016-12-25 14:22:21 +0200

    Added first unit tests

---
+++ BA.ScrumPoker.Web.Tests/RoomsTheBetterOneTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using BA.ScrumPoker.MemoryData;
+using NUnit.Framework;
+
+namespace BA.ScrumPoker.Web.Tests
+{
+	[TestFixture]
+	public class RoomsTheBetterOneTests
+	{
+
+		[Test]
+		public void CreateRoom_Should_Create_Room()
+		{
+			// arrange
+			var rooms = RoomsTheBetterOne.Instance;
+
+			// act
+			var room = rooms.CreateRoom();
+
+			// assert
+			Assert.IsNotNull(room);
+		}
+
+		[Test]
+		public void JoinRoom_Should_Create_Client_In_Room()
+		{
+			// arrange
+			var rooms = RoomsTheBetterOne.Instance;
+			var username = "TestUSername";
+
+			// act
+			var room = rooms.CreateRoom();
+			var client = rooms.JoinRoom(username, room.RoomId);
+
+			// assert
+			Assert.IsNotNull(client);
+			Assert.AreEqual(client.RoomId, room.RoomId);
+
+			Assert.AreEqual(room.Clients.First(), client);
+		}
+	}
+}
