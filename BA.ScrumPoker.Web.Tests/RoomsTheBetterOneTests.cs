45e5307888cdeb0a471a0a26d575dee165e437a9
Tadas Ščerbickas <t.scerbickas@baltic-amadeus.lt>
2016-12-25 14:24:54 +0200

    another test method

--- BA.ScrumPoker.Web.Tests/RoomsTheBetterOneTests.cs
+++ BA.ScrumPoker.Web.Tests/RoomsTheBetterOneTests.cs
@@ -35,8 +35,27 @@ namespace BA.ScrumPoker.Web.Tests
 			// assert
 			Assert.IsNotNull(client);
 			Assert.AreEqual(client.RoomId, room.RoomId);
+			Assert.AreEqual(client.Name, username);
 
 			Assert.AreEqual(room.Clients.First(), client);
+		}
+
+		[Test]
+		public void KickClient_Should_Remove_Client_From_Room()
+		{
+			// arrange
+			var rooms = RoomsTheBetterOne.Instance;
+			var username = "TestUSername";
+
+			// act
+			var room = rooms.CreateRoom();
+			var client = rooms.JoinRoom(username, room.RoomId);
+
+			var kicked = rooms.Kick(room.RoomId, room.SecretKey, client.ClientId);
+
+			// assert
+			Assert.IsTrue(kicked);
+			Assert.IsTrue(room.Clients.Count == 0);
 		}
 	}
 }
